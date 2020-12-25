57b480ef5775b7b4eb90814192cc7ea7c4ea1df5
yeasin <m.yeasin@graduate.utm.my>
2020-12-25 23:17:05 +0800

    EJS implemnted with route handleing

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const speakerRoute = require('./speakers')
+const feedbackRoute = require('./feedback')
+
+const router = express.Router()
+
+module.exports = () => {
+    router.get('/', (req, res) => {
+        res.render('pages/index', {
+            pageTitle: 'welcome'
+        })
+    })
+
+    router.use('/speakers', speakerRoute())
+
+    router.use('/feedback', feedbackRoute())
+
+    return router
+}
